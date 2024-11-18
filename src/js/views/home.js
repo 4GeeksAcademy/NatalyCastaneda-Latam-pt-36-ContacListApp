a3dab5c6e4d4f16719788a7921f4ef766d212082
Nath <115433779+Nath1710@users.noreply.github.com>
2024-11-18 19:18:28 +0000

    Error correction in the execution of the get method

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -11,7 +11,7 @@ export const Home = () => {
 		if (store.contacts && store.contacts.length === 0) {
 			actions.getAgendaContacts();
 		}
-	}, [store.contacts]);
+	}, []);
 
 	return (
 		<div className="text-center mt-5">
